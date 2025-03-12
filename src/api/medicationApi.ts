6e541c65dd0f7232c7e27830f0cb5af33cbc79d3
Onda Reki <rekionda0524@ezweb.ne.jp>
2025-03-13 00:14:11 +0900

    postにIDトークン追加

--- src/api/medicationApi.ts
+++ src/api/medicationApi.ts
@@ -1,3 +1,5 @@
+import liff from "@line/liff";
+
 const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 export interface MedicationData {
@@ -8,10 +10,15 @@ export interface MedicationData {
 
 export const medicationApi = {
   create: async (data: MedicationData) => {
+    await liff.init({ liffId: import.meta.env.VITE_LIFF_ID });
+    // ログイン後、IDトークンを取得
+    const idToken = liff.getIDToken();
+
     const response = await fetch(`${API_BASE_URL}/medications`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        Authorization: `Bearer ${idToken}`,
       },
       body: JSON.stringify(data),
     });
